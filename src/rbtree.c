345584dd1e0fb137971be386e4b35cea311241ad
ddoavoca <ddoavoca@gmail.com>
2024-10-14 21:44:08 +0900

    new_rbtree
    new_rbtree함수 코드 작성

--- src/rbtree.c
+++ src/rbtree.c
@@ -1,44 +1,41 @@
 #include "rbtree.h"
-
 #include <stdlib.h>
 
 rbtree *new_rbtree(void) {
-  rbtree *p = (rbtree *)calloc(1, sizeof(rbtree));
-  // TODO: initialize struct if needed
+  rbtree* p = (rbtree*)calloc(1, sizeof(rbtree)); // 트리 구조체 메모리 할당
+  node_t* nil_node = (node_t*)calloc(1, sizeof(node_t)); // sentinel 노드(nil) 생성
+ 
+  nil_node->color = RBTREE_BLACK;  // sentinel 노드는 항상 검정색
+  p->nil = nil_node;
+  p->root = p->nil;  // 빈 트리는 root가 nil을 가리킴
+
   return p;
 }
 
 void delete_rbtree(rbtree *t) {
-  // TODO: reclaim the tree nodes's memory
-  free(t);
+   free(t);
 }
 
 node_t *rbtree_insert(rbtree *t, const key_t key) {
-  // TODO: implement insert
   return t->root;
 }
 
 node_t *rbtree_find(const rbtree *t, const key_t key) {
-  // TODO: implement find
   return t->root;
 }
 
 node_t *rbtree_min(const rbtree *t) {
-  // TODO: implement find
   return t->root;
 }
 
 node_t *rbtree_max(const rbtree *t) {
-  // TODO: implement find
   return t->root;
 }
 
 int rbtree_erase(rbtree *t, node_t *p) {
-  // TODO: implement erase
   return 0;
 }
 
 int rbtree_to_array(const rbtree *t, key_t *arr, const size_t n) {
-  // TODO: implement to_array
   return 0;
 }
